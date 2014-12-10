5c3d323d12720380e3cb4ce0378bfb30b4eee223
Xue-Chun Chen <Xue-Chun.Chen@td.com>
2014-12-10 11:38:30 -0500

    test1 new

---
+++ src/java/com/xtest/play/HW.java
@@ -0,0 +1,30 @@
+package xtest.hello;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by chenxa on 11/14/2014.
+ */
+public class HW {
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE  );
+
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add( label );
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize( new Dimension(300, 100) );
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater( ()-> {
+            createAndShowGUI();
+        });
+     }
+
+}
+
